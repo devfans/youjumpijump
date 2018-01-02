52265b314dd36e068c342b1300bf3375bff6c3ae
左进民 <312093933@qq.com>
2018-01-02 12:49:08 +0800

    修复了iPhone8跳跃不准的问题，重新设定了跳跃系数，

--- ios/main.go
+++ ios/main.go
@@ -68,7 +68,7 @@ func main() {
 	}
 
 	var inputRatio float64
-	fmt.Print("请输入跳跃系数(推荐值 2，可适当调整):")
+	fmt.Print("请输入跳跃系数(推荐值 3.857，可适当调整):")
 	_, err = fmt.Scanln(&inputRatio)
 	if err != nil {
 		log.Fatal(err)
@@ -86,47 +86,64 @@ func main() {
 		f.Close()
 
 		start, end := jump.Find(src)
+		 isjump := true
+		//game over 检测不到游戏 命令行提示 可以手动调用系统的关闭重开方法，待更新
 		if start == nil {
-			log.Print("找不到起点，请把 debugger 目录打包发给开发者检查问题。")
-			break
+			log.Print("请重新开始游戏，检测不到游戏画面")
+			isjump = false
+			//break
 		} else if end == nil {
-			log.Print("找不到落脚点，请把 debugger 目录打包发给开发者检查问题。")
-			break
+			isjump = false
+			log.Print("请重新开始游戏，检测不到游戏画面")
+			//break
 		}
+		if isjump {
+			nowDistance := jump.Distance(start, end)
+			similarDistance, nowRatio := similar.Find(nowDistance)
 
-		nowDistance := jump.Distance(start, end)
-		similarDistance, nowRatio := similar.Find(nowDistance)
+			log.Printf("起点位置:%v 结束位置:%v 距离:%.2f 系数:%.2f 变换后的系数:%v 跳跃时间:%.2fms =xxx%v", start, end, nowDistance, similarDistance, nowRatio, nowDistance*nowRatio,nowDistance * nowRatio / 1000)
+			// 跳跃时间
+			var jumptime float64
+			//fmt.Print("请输入跳跃时间(可适当调整):")
+			//_, err = fmt.Scanln(&jumptime)
+			//fmt.Println("跳跃时间====",jumptime)
+			//主要修改了跳跃系数
+			jumptime = nowDistance/123.54/inputRatio;
 
-		log.Printf("from:%v to:%v distance:%.2f similar:%.2f ratio:%v press:%.2fms ", start, end, nowDistance, similarDistance, nowRatio, nowDistance*nowRatio)
 
-		_, _, err = r.PostJSON(fmt.Sprintf("http://%s/session/%s/wda/touchAndHold", ip, res.SessionID), map[string]interface{}{
-			"x":        200,
-			"y":        200,
-			"duration": nowDistance * nowRatio / 1000,
-		})
-		if err != nil {
-			panic(err)
-		}
+			fmt.Println("距离时间=====",nowDistance/123.54/inputRatio);
+			_, _, err = r.PostJSON(fmt.Sprintf("http://%s/session/%s/wda/touchAndHold", ip, res.SessionID), map[string]interface{}{
+				"x":        200,
+				"y":        200,
+				"duration": jumptime,
 
-		go func() {
-			time.Sleep(time.Duration(nowDistance*nowRatio/1000+50) * time.Millisecond)
-			_, src := screenshot(ip)
-			finally, _ := jump.Find(src)
-
-			f, _ = os.OpenFile("jump.test.png", os.O_WRONLY|os.O_CREATE, 0600)
-			png.Encode(f, src)
-			f.Close()
-
-			if finally != nil {
-				finallyDistance := jump.Distance(start, finally)
-				finallyRatio := (nowDistance * nowRatio) / finallyDistance
-
-				if finallyRatio > nowRatio/2 && finallyRatio < nowRatio*2 {
-					similar.Add(finallyDistance, finallyRatio)
-				}
+					///nowDistance * nowRatio / 1000,
+			})
+			if err != nil {
+				panic(err)
 			}
-		}()
 
-		time.Sleep(time.Millisecond * 1500)
+			//go func() {
+			//	time.Sleep(time.Duration(nowDistance*nowRatio/1000+50) * time.Millisecond)
+			//	_, src := screenshot(ip)
+			//	finally, _ := jump.Find(src)
+			//
+			//	f, _ = os.OpenFile("jump.test.png", os.O_WRONLY|os.O_CREATE, 0600)
+			//	png.Encode(f, src)
+			//	f.Close()
+			//
+			//	if finally != nil {
+			//		finallyDistance := jump.Distance(start, finally)
+			//		finallyRatio := (nowDistance * nowRatio) / finallyDistance
+			//
+			//		if finallyRatio > nowRatio/2 && finallyRatio < nowRatio*2 {
+			//			similar.Add(finallyDistance, finallyRatio)
+			//		}
+			//	}
+			//}()
+
+			time.Sleep(time.Millisecond * 1500)
+		}
+
 	}
 }
